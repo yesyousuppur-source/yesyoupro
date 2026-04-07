e290c4e4bca2ef73c5b91c1f0a62d9394b1fe1db
yesyousuppur-source <yesyousuppur@gmail.com>
2026-04-07 21:54:14 +0530

    Create _document.js

---
+++ pages/_document.js
@@ -0,0 +1,12 @@
+import { Html, Head, Main, NextScript } from "next/document";
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head>
+        <meta name="description" content="YesYouPro - AI product analyzer for Indian ecommerce sellers." />
+        <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
+      </Head>
+      <body><Main /><NextScript /></body>
+    </Html>
+  );
+}
