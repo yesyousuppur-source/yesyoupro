24b91e8dce960d1531592cb670654dd6f079a2f9
yesyousuppur-source <yesyousuppur@gmail.com>
2026-04-08 21:43:54 +0530

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -1,12 +1,34 @@
 import { Html, Head, Main, NextScript } from "next/document";
+
 export default function Document() {
   return (
     <Html lang="en">
       <Head>
         <meta name="description" content="YesYouPro - AI product analyzer for Indian ecommerce sellers." />
         <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
+
+        {/* Google Analytics */}
+        <script
+          async
+          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_ID}`}
+        />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', '${process.env.NEXT_PUBLIC_GA_ID}', {
+                page_path: window.location.pathname,
+              });
+            `,
+          }}
+        />
       </Head>
-      <body><Main /><NextScript /></body>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
     </Html>
   );
 }
