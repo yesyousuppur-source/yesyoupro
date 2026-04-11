17ead1996e6c5d543afb4db2e053f96eabd84901
yesyousuppur-source <yesyousuppur@gmail.com>
2026-04-11 12:09:47 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,5 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
   async headers() {
     return [
       {
@@ -12,4 +18,5 @@ const nextConfig = {
     ];
   },
 };
+
 module.exports = nextConfig;
