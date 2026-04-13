40856cb5824247e744f3f8fd28bc932cf8efc384
yesyousuppur-source <yesyousuppur@gmail.com>
2026-04-13 19:04:31 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -6,19 +6,8 @@ const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
-  images: {
-    domains: ['cdn.simpleicons.org', 'lh3.googleusercontent.com'],
-  },
-  async headers() {
-    return [
-      {
-        source: "/(.*)",
-        headers: [
-          { key: "X-Frame-Options", value: "SAMEORIGIN" },
-          { key: "X-Content-Type-Options", value: "nosniff" },
-        ],
-      },
-    ];
+  generateBuildId: async () => {
+    return 'build-' + Date.now();
   },
 };
 
