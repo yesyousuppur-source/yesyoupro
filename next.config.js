6543d99ebdd44c1f067c98693ae827ebf7703924
yesyousuppur-source <yesyousuppur@gmail.com>
2026-04-15 12:14:35 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -6,6 +6,17 @@ const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          { key: "Content-Type", value: "text/html; charset=utf-8" },
+          { key: "X-Content-Type-Options", value: "nosniff" },
+        ],
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
